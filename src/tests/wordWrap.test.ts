298cccafd88d0616b25131c5d664f271ef70490d
yessica <yessica@leanmind.es>
2022-07-04 16:30:05 +0100

    RED: separate column is created when the word is wider than the column

--- src/tests/wordWrap.test.ts
+++ src/tests/wordWrap.test.ts
@@ -1,7 +1,7 @@
 import { wordWrapCalculator } from '../core/wordWrap';
 
 // wordWrap('',5) -> '' --> DONE
-// wordWrap('hello',5) -> 'hello'
+// wordWrap('hello',5) -> 'hello' --> DONE
 // wordWrap('longword',4) -> 'long\nword'
 // wordWrap('reallylongword',4) ->'real\nlylo\nngwo\nrd'
 // wordWrap('abc def',4) -> 'abc\ndef' 
@@ -18,5 +18,9 @@ describe('Word wrap', () => {
 
   it('returns complete word when is not longer than column wide', () => {
     expect(wordWrapCalculator('hello',5)).toEqual('hello');
+  });
+
+  it('separate column is created when the word is wider than the column', () => {
+    expect(wordWrapCalculator('longword',4)).toEqual('long\nword');
   });
 });
