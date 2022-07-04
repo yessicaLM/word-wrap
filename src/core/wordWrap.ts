7785edd119ccb2d67009b01266b08438ed0e65fa
yessica <yessica@leanmind.es>
2022-07-04 17:34:51 +0100

    GREEN: negative numbers for columns separators are not allowed

--- src/core/wordWrap.ts
+++ src/core/wordWrap.ts
@@ -2,21 +2,25 @@ export const wordWrapCalculator = (stringValue: string, columnWide: number): str
   if (!stringValue) return '';
   if (stringValue.length <= columnWide) return stringValue;
 
-  const listOfWords = [];
-  let columnLimit = columnWide;
+  if (columnWide > 0) {
+    const listOfWords = [];
+    let columnLimit = columnWide;
 
-  while (stringValue.length > 0) {
-    let word = stringValue.substring(0, columnLimit);
-    if (word.charAt(0) === ' ') {
-      columnLimit ++;
-      word = stringValue.substring(1, columnLimit);
-      word = '\n' + word;
+    while (stringValue.length > 0) {
+      let word = stringValue.substring(0, columnLimit);
+      if (word.charAt(0) === ' ') {
+        columnLimit++;
+        word = stringValue.substring(1, columnLimit);
+        word = '\n' + word;
+      }
+      if (word.charAt(word.length - 1) === ' ') {
+        word = word.trimEnd();
+      }
+      listOfWords.push(word);
+      stringValue = stringValue.substring(columnLimit);
     }
-    if (word.charAt(word.length - 1) === ' ') {
-      word = word.trimEnd();      
-    }
-    listOfWords.push(word);
-    stringValue = stringValue.substring(columnLimit);
+    return listOfWords.join('\n');
+  } else {
+    throw new Error('Negative numbers are not allowed');
   }
-  return listOfWords.join('\n');
 }
