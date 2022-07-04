3e5b30b00d2b420e95738dcb323c7b429e0f995d
yessica <yessica@leanmind.es>
2022-07-04 16:56:39 +0100

    GREEN: separate columns are created when the words are wider than the columns

--- src/core/wordWrap.ts
+++ src/core/wordWrap.ts
@@ -1,5 +1,13 @@
 export const wordWrapCalculator = (stringValue: string, columnWide: number): string => {
   if (stringValue === '') return '';
   if (stringValue.length <= columnWide) return stringValue;
-  return [stringValue.slice(0, columnWide), stringValue.slice(columnWide)].join('\n');
+
+  const listOfWords = [];
+
+  while (stringValue.length > 0) {
+    const word = stringValue.substring(0, columnWide);
+    listOfWords.push(word);
+    stringValue = stringValue.substring(columnWide);
+  }
+  return listOfWords.join('\n');
 }
