6ea6ca7b9b0429d060444779f587282f1da7c07e
yessica <yessica@leanmind.es>
2022-07-04 17:25:08 +0100

    GREEN: a blank space at the beginning of the word creates new a column

--- src/core/wordWrap.ts
+++ src/core/wordWrap.ts
@@ -3,14 +3,20 @@ export const wordWrapCalculator = (stringValue: string, columnWide: number): str
   if (stringValue.length <= columnWide) return stringValue;
 
   const listOfWords = [];
+  let columnLimit = columnWide;
 
   while (stringValue.length > 0) {
-    let word = stringValue.substring(0, columnWide);
+    let word = stringValue.substring(0, columnLimit);
+    if (word.charAt(0) === ' ') {
+      columnLimit ++;
+      word = stringValue.substring(1, columnLimit);
+      word = '\n' + word;
+    }
     if (word.charAt(word.length - 1) === ' ') {
       word = word.trimEnd();      
     }
     listOfWords.push(word);
-    stringValue = stringValue.substring(columnWide);
+    stringValue = stringValue.substring(columnLimit);
   }
   return listOfWords.join('\n');
 }
