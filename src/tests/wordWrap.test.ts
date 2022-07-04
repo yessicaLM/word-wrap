897f2e911b4a02bc6efe377efa5826cfebeed358
yessica <yessica@leanmind.es>
2022-07-04 17:08:50 +0100

    RED: a blank space at the beginning of the word creates new a column

--- src/tests/wordWrap.test.ts
+++ src/tests/wordWrap.test.ts
@@ -5,8 +5,8 @@ import { wordWrapCalculator } from '../core/wordWrap';
 // wordWrap('longword',4) -> 'long\nword' --> DONE
 // wordWrap('reallylongword',4) ->'real\nlylo\nngwo\nrd' -->DONE
 // wordWrap('abc def',4) -> 'abc\ndef' --> DONE
-// wordWrap('abc def ghi',4) -> 'abc\ndef\nghi'
-// wordWrap('' abcdf',4) -> '\nabcd'
+// wordWrap('abc def ghi',4) -> 'abc\ndef\nghi' --> DONE
+// wordWrap(' abcdf',4) -> '\nabcd'
 // wordWrap(null,5) ->''
 // wordWrap('hello',-5) -> throw exception
 
@@ -31,5 +31,9 @@ describe('Word wrap', () => {
   it('a blank space just before the cut creates new a column', () => {
     expect(wordWrapCalculator('abc def',4)).toEqual('abc\ndef');
     expect(wordWrapCalculator('abc def ghi',4)).toEqual('abc\ndef\nghi');
+  });
+
+  it('a blank space at the beginning of the word creates new a column', () => {
+    expect(wordWrapCalculator(' abcdf',4)).toEqual('\nabcd\nf');
   });
 });
